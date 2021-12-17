383000c9593e31f92a63828e80ae8fac13738ea1
enrique1994mex <enrique.antonio1029@gmail.com>
2021-12-17 14:04:21 -0600

    se crearon las rutas de los productos

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Layout from '../components/Layout'; 
-import {Grid, Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button} from '@mui/material'; 
+import Layout from '../components/Layout';
+import { Grid, Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button } from '@mui/material';
 import data from '../utils/data';
- 
+import NextLink from 'next/link';
 
 export default function Home() {
   return (
@@ -13,12 +13,14 @@ export default function Home() {
             data.products.map((product) => (
               <Grid item md={4} key={product.name}>
                 <Card>
-                  <CardActionArea>
-                    <CardMedia component="img" image={product.image} title={product.name}></CardMedia>
-                    <CardContent>
-                      <Typography>{product.name}</Typography>
-                    </CardContent>
-                  </CardActionArea>
+                  <NextLink href={`/product/${product.slug}`} passHref>
+                    <CardActionArea>
+                      <CardMedia component="img" image={product.image} title={product.name}></CardMedia>
+                      <CardContent>
+                        <Typography>{product.name}</Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </NextLink>
                   <CardActions>
                     <Typography>{product.price}</Typography>
                     <Button size="small" color="primary">
