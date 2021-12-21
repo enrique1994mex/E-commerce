d4d0ba4b6c43e2b8cfed9d702ee0229333417f17
enrique1994mex <enrique.antonio1029@gmail.com>
2021-12-20 21:53:11 -0600

    Se creo el componente cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,8 +7,10 @@ import Product from '../../models/Product';
 import db from '../../utils/db';
 import axios from 'axios';
 import {Store} from '../../utils/Store'; 
+import {useRouter} from 'next/router'; 
 
 export default function ProductScreen(props) {
+    const router = useRouter();
     const {dispatch} = useContext(Store); 
     const {product} = props; 
     if(!product) {
@@ -22,6 +24,7 @@ export default function ProductScreen(props) {
             return; 
         }
         dispatch({ type: 'CART_ADD_ITEM', payload: {...product, quantity: 1} }); 
+        router.push('/cart');
     }
     return (
         <Layout title={product.name} description={product.description}>
