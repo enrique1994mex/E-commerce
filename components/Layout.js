d9c645fbfc9aa6f6864c3562785544011a7e39dc
enrique1994mex <enrique.antonio1029@gmail.com>
2021-12-22 17:54:01 -0600

    Se acompleto el componente Login

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,16 @@
-import React, {useContext} from 'react';
+import React, { useContext, useState } from 'react';
 import Head from 'next/head';
-import { AppBar, Toolbar, Typography, Container, Link, createTheme, ThemeProvider, CssBaseline, Switch, Badge } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, Link, createTheme, ThemeProvider, CssBaseline, Switch, Badge, Button, Menu, MenuItem } from '@mui/material';
 import useStyles from '../utils/styles';
 import NextLink from 'next/link';
-import {Store} from '../utils/Store';
-import Cookies from 'js-cookie'; 
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
+import {useRouter} from 'next/router'; 
 
 export default function Layout({ title, description, children }) {
-    const {state, dispatch} = useContext(Store); 
-    const {darkMode, cart} = state; 
+    const router = useRouter();
+    const { state, dispatch } = useContext(Store);
+    const { darkMode, cart, userInfo } = state;
     const theme = createTheme({
         typography: {
             h1: {
@@ -26,20 +28,34 @@ export default function Layout({ title, description, children }) {
             },
         },
         palette: {
-            mode: darkMode? 'dark' : 'light',
+            mode: darkMode ? 'dark' : 'light',
             primary: {
                 main: '#f0c000',
             },
             secondary: {
                 main: '#208080',
             },
-        }, 
+        },
     });
     const classes = useStyles();
     const darkModeChangeHandler = () => {
-        dispatch({type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON'});
+        dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
         const newDarkMode = !darkMode;
-        Cookies.set('darkMode', newDarkMode? 'ON' : 'OFF'); 
+        Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+    }
+    const [anchorEl, setAnchorEl] = useState(null);  
+    const loginClickHandler = (e) => {
+        setAnchorEl(e.currentTarget); 
+    }
+    const loginMenuCloseHandler = () => {
+        setAnchorEl(null); 
+    }
+    const logoutClickHandler = () => {
+        setAnchorEl(null);
+        dispatch({type: 'USER_LOGOUT'}); 
+        Cookies.remove('userInfo'); 
+        Cookies.remove('cartItems'); 
+        router.push('/'); 
     }
     return (
         <div>
@@ -50,7 +66,7 @@ export default function Layout({ title, description, children }) {
                 </title>
             </Head>
             <ThemeProvider theme={theme}>
-                <CssBaseline/>
+                <CssBaseline />
                 <AppBar position='static' className={classes.navBar}>
                     <Toolbar>
                         <NextLink href="/" passHref>
@@ -63,12 +79,36 @@ export default function Layout({ title, description, children }) {
                             <Switch checked={darkMode} onChange={darkModeChangeHandler}></Switch>
                             <NextLink href="/cart" passHref>
                                 <Link>
-                                    {cart.cartItems.length > 0 ? <Badge color="secondary" badgeContent={cart.cartItems.length}>Cart</Badge>: "Cart"}
+                                    {cart.cartItems.length > 0 ? <Badge color="secondary" badgeContent={cart.cartItems.length}>Cart</Badge> : "Cart"}
                                 </Link>
                             </NextLink>
-                            <NextLink href="/login" passHref>
-                                <Link>Login</Link>
-                            </NextLink>
+                            {userInfo ? (
+                                <>
+                                    <Button id="basic-button"
+                                        aria-controls="simple-menu"
+                                        aria-haspopup="true"
+                                        onClick={loginClickHandler} 
+                                        className={classes.navbarButton}>
+                                        {userInfo.name}
+                                    </Button>
+                                    <Menu
+                                        id="simple-menu"
+                                        anchorEl={anchorEl}
+                                        keepMounted
+                                        open={Boolean(anchorEl)}
+                                        onClose={loginMenuCloseHandler}
+                                        
+                                    >
+                                        <MenuItem onClick={loginMenuCloseHandler}>Profile</MenuItem>
+                                        <MenuItem onClick={loginMenuCloseHandler}>My account</MenuItem>
+                                        <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
+                                    </Menu>
+                                </>
+                            ) : (
+                                <NextLink href="/login" passHref>
+                                    <Link>Login</Link>
+                                </NextLink>
+                            )}
                         </div>
                     </Toolbar>
                 </AppBar>
--- utils/Store.js
+++ utils/Store.js
@@ -1,12 +1,13 @@
 import { createContext, useReducer } from "react";
-import Cookies from 'js-cookie'; 
+import Cookies from 'js-cookie';
 
 export const Store = createContext();
 const initialState = {
-    darkMode: Cookies.get('darkMode') === 'ON' ? true : false,  
+    darkMode: Cookies.get('darkMode') === 'ON' ? true : false,
     cart: {
-        cartItems: Cookies.get('cartItems')? JSON.parse(Cookies.get('cartItems')) :  [],
-    }
+        cartItems: Cookies.get('cartItems') ? JSON.parse(Cookies.get('cartItems')) : [],
+    },
+    userInfo: Cookies.get('userInfo') ? JSON.parse(JSON.stringify(Cookies.get('userInfo'))) : null,
 };
 
 function reducer(state, action) {
@@ -14,27 +15,31 @@ function reducer(state, action) {
         case 'DARK_MODE_ON':
             return { ...state, darkMode: true };
         case 'DARK_MODE_OFF':
-            return { ...state, darkMode: false }; 
+            return { ...state, darkMode: false };
         case 'CART_ADD_ITEM': {
-            const newItem = action.payload; 
+            const newItem = action.payload;
             const existItem = state.cart.cartItems.find(item => item._id === newItem._id);
-            const cartItems = existItem? state.cart.cartItems.map(item => item.name === existItem.name?newItem: item)
-            : [...state.cart.cartItems, newItem]; 
+            const cartItems = existItem ? state.cart.cartItems.map(item => item.name === existItem.name ? newItem : item)
+                : [...state.cart.cartItems, newItem];
             Cookies.set('cartItems', JSON.stringify(cartItems))
-            return {...state, cart: {...state.cart, cartItems}}; 
+            return { ...state, cart: { ...state.cart, cartItems } };
         }
         case 'CART_REMOVE_ITEM': {
             const cartItems = state.cart.cartItems.filter(item => item._id !== action.payload._id);
             Cookies.set('cartItems', JSON.stringify(cartItems))
-            return {...state, cart: {...state.cart, cartItems}}; 
+            return { ...state, cart: { ...state.cart, cartItems } };
         }
+        case 'USER_LOGIN':
+            return { ...state, userInfo: action.payload };
+        case 'USER_LOGOUT':
+            return { ...state, userInfo: null, cart: {cartItems: []} }; 
         default:
-            return state; 
+            return state;
     }
 }
 
 export function StoreProvider(props) {
     const [state, dispatch] = useReducer(reducer, initialState);
-    const value = {state, dispatch};
+    const value = { state, dispatch };
     return <Store.Provider value={value}>{props.children}</Store.Provider>
 }
