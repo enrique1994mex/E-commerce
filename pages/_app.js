9a399289e3103161878c9807ed7ee00f07996420
enrique1994mex <enrique.antonio1029@gmail.com>
2021-12-23 23:30:26 -0600

    Se agregaron validaciones a los dos formularios

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,21 @@
 import '../styles/globals.css';
-import {useEffect} from 'react'; 
-import {StoreProvider} from '../utils/Store';
+import { useEffect } from 'react';
+import { StoreProvider } from '../utils/Store';
+import { SnackbarProvider } from 'notistack';
 
 function MyApp({ Component, pageProps }) {
-  useEffect( () => {
-    const jssStyles = document.querySelector('#jss-server-side'); 
-    if(jssStyles) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, [])
   return (
-    <StoreProvider>
-      <Component {...pageProps} />
-    </StoreProvider>
+    <SnackbarProvider anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
+    </SnackbarProvider>
   )
 }
 
