3741192e9f9f50adb211290616cb27e113091094
enrique1994mex <enrique.antonio1029@gmail.com>
2021-12-25 19:45:17 -0600

    Se agrego el componente Shipping y su funcionalidad

---
+++ components/checkoutWizard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Stepper, Step, StepLabel} from '@mui/material';
+
+export default function CheckoutWizard({ activeStep = 0}) {
+    return (
+        <Stepper activeStep={activeStep} alternativeLabel>
+           { ['Login', 'Shipping Address', 'Payment Method', 'Place Order'].map(step => (
+               <Step key={step}>
+                   <StepLabel>{step}</StepLabel>
+               </Step>
+           ))}
+        </Stepper>
+    )
+}
